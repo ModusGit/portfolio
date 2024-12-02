c96e5fd79e4a209acf3eb1926b0bd3bed242936a
ModusGit <157380395+ModusGit@users.noreply.github.com>
2024-12-02 17:48:28 +0100

    modal boxes in project section ok with different id

--- assets/js/main.js
+++ assets/js/main.js
@@ -176,26 +176,34 @@ filters.forEach(filter => {
     });
 });
 
-// Get the modal
-var modal = document.getElementById("myProjectModal");
-// Get the button that opens the modal
-var btn = document.getElementById("myProjectBtn");
-// Get the <span> element that closes the modal
-var span = document.querySelector(".modal .close");
-// When the user clicks on the button, open the modal
-btn.addEventListener("click", function() {
-    modal.style.display = "block";
+// Sélectionner tous les boutons et leurs modals associés
+var buttons = document.querySelectorAll(".btn-middle-image-uniforme");
+var modals = document.querySelectorAll(".modal");
+var closeButtons = document.querySelectorAll(".close");
+
+// Ajouter des événements pour ouvrir chaque modal
+buttons.forEach((button, index) => {
+    button.addEventListener("click", function() {
+        modals[index].style.display = "block";
+    });
 });
-// When the user clicks on <span> (x), close the modal
-span.addEventListener("click", function() {
-    modal.style.display = "none";
+
+// Ajouter des événements pour fermer chaque modal
+closeButtons.forEach((close, index) => {
+    close.addEventListener("click", function() {
+        modals[index].style.display = "none";
+    });
 });
-// When the user clicks anywhere outside of the modal, close it
+
+// Fermer le modal si on clique en dehors
 window.addEventListener("click", function(event) {
-    if (event.target === modal) {
-        modal.style.display = "none";
-    }
+    modals.forEach((modal) => {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    });
 });
+
 
 
 
